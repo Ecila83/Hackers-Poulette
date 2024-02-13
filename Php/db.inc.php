b49c9eac22c688fa84b97c400b77ec23876f0040
Alice Marique <mariquealice83@gmail.com>
2024-02-13 10:47:22 +0100

    connecte base de donnée

---
+++ Php/db.inc.php
@@ -0,0 +1,11 @@
+<?php
+function connect_db() {
+    $dsn = 'mysql:dbname=hackers_poulette;host=db';
+    $user = 'root';
+    $password = 'KWmuLNjpzseXcTUNcnpz';
+    $pdo = new PDO($dsn, $user, $password);
+
+    return $pdo;
+}
+?>
+
