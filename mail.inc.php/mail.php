49a301d00abc37fdd66b3d53c672f1f74db48d49
Hanen Wechteti <hanenwechteti27@gmail.com>
2024-02-15 15:26:52 +0100

    version4

---
+++ mail.inc.php/mail.php
@@ -0,0 +1,42 @@
+<?php
+
+$nom = $_POST['nom'];
+$email= $_POST['mail'];
+$message= $_POST['description'];
+
+$message = "nom : ".$nom."\n"." Email : ".$email."\n"." message : ".$message;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+require 'path/to/PHPMailer/src/SMTP.php';
+ 
+
+    $mail = new PHPMailer(true);
+    try{
+$mail->IsSMTP();
+$mail->Host = 'smtp.gmail.com';  //le serveur smtp de gmail            
+$mail->SMTPAuth = true;
+$mail->Username = 'hanenwechteti27@gmail.com';  //mail: permet d'acceder a attriburt user name de l'object creer
+$mail->Password = 'ztsy naxp xvvj wcos'; // le mot de passe que je viens de le creer
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->Port = 465;                       
+
+//content
+//$mail->setFrom($from_email, $from_name); //le message est envoyé par $from_email
+//$mail->addAdress('hanenwechtetii12@gmail.com','');
+$mail->isHTML(true);
+$mail->Subject ='here is the subject';
+$mail->Body    =$message;
+$mail->AltBody ='this is the body in the plain text for non-HTML mail clients';
+//$mail->setLanguage('fr','/optional/path/to/language/directory/'); // pour charger la version francaise
+$mail->send();
+echo'message has been sent';
+    } catch (Exception $e) {
+        echo "message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+
+?>
